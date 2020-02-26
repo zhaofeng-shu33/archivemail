f6fdc2f775ebdc5a1f09a7bc19102b8e10f31858
zhaofeng-shu33 <616545598@qq.com>
2020-02-26 22:50:59 +0800

    fix lint problems

--- manipulate.py
+++ manipulate.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-# pylint: disable=missing-module-docstring
 import os
 import argparse
 
@@ -8,35 +7,33 @@ from email.header import decode_header
 from email.utils import parsedate
 
 OUTPUT_DIR = 'read'
-def write_dic(dic):
-    tuple_obj = parsedate(dic['time'])
+def write_dic(_dic):
+    tuple_obj = parsedate(_dic['time'])
     year = str(tuple_obj[0])
     month = tuple_obj[1]
     day = tuple_obj[2]
     if not os.path.exists(os.path.join(OUTPUT_DIR, year)):
         os.mkdir(os.path.join(OUTPUT_DIR, year))
     str_tim = '%s-%d-%d' % (year, month, day)
-    file_name = str_tim + '_' + dic['subject'] + '.md'
-    content_all = dic['subject'] + '\n\n'
-    content_all += dic['from'] + '\n\n'
-    content_all += dic['content']
+    file_name = str_tim + '_' + _dic['subject'] + '.md'
+    content_all = _dic['subject'] + '\n\n'
+    content_all += _dic['from'] + '\n\n'
+    content_all += _dic['content']
     with open(os.path.join(OUTPUT_DIR, year, file_name), 'w') as f:
         f.write(content_all)
-    return
 
-def get_decode_content(message):
-    _contents = message.get_payload(decode=True)
-    charset = message.get_content_charset()
+def get_decode_content(_message):
+    _contents = _message.get_payload(decode=True)
+    charset = _message.get_content_charset()
     if charset is None:
         try:
             _contents = _contents.decode('utf-8')
-        except:
-            try:
-                _contents = _contents.decode('gbk')
+        except UnicodeDecodeError:
+            _contents = _contents.decode('gbk')
     else:
         try:
             _contents = _contents.decode(charset)
-        except:
+        except UnicodeDecodeError:
             _contents = _contents.decode('gbk')
     return _contents
 
@@ -86,4 +83,3 @@ if __name__ == '__main__':
         dic['content'] = contents
         if not args.dry_run:
             write_dic(dic)
- 
